e7923169a4520105ba7b4a3cd5a439e603b0e52d
Nicholas Keller <nic.keller328@gmail.com>
2020-04-07 20:18:35 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,63 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  attr_writer 
+  
+  @@all = []
+  
+  def initialize(name, species="human")
+    @name = name
+    @species = species
+    @@all << self
+  end
+  
+  def name
+    @name
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.cats
+    @cats = []
+    Cat.all.each do |cat|
+      if cat.owner == self
+        @cats << cat
+      else
+      end
+    end
+      @cat 
+  end
+
+  def self.dogs
+    @dogs = []
+    Dog.all.each do |dog|
+      if dog.owner == self
+        @dogs << dog
+      else
+      end
+    end
+      @dog 
+  end
+  
+  def buy_cat(new_cat)
+    new_cat.owner = self
+  end
+
+  def buy_dog(new_dog)
+    new_dog.owner = self
+  end
+  
 end
